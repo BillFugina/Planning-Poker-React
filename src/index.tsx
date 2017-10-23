664094d198c6b4caa7e69390b0e8b2fafdc3b640
Bill Fugina <bill@dogspots.com>
2017-10-23 19:04:01 -0400

    install bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap-theme.css'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  ,
   document.getElementById('root') as HTMLElement
 )
 registerServiceWorker()
