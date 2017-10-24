7ca02ddf8ae6d59309bb9f523a83740e25a4c299
Bill Fugina <bill@dogspots.com>
2017-10-24 11:16:46 -0400

    eject

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import './App.css'
+import 'App.css'
 import { Navbar, Jumbotron } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap-theme.css'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+import App from 'App'
+import registerServiceWorker from 'registerServiceWorker'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
