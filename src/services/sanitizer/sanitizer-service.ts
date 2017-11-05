234847a3747c45e2b4f17d24933f84630d833087
Bill Fugina <bill@dogspots.com>
2017-11-05 00:09:48 -0400

    fix dependency injection

--- src/services/sanitizer/sanitizer-service.ts
+++ src/services/sanitizer/sanitizer-service.ts
@@ -1,4 +1,8 @@
-export class SanitizerService {
+import { injectable } from 'inversify'
+import { ISanitizerService } from 'services/sanitizer'
+
+@injectable()
+export class SanitizerService implements ISanitizerService {
     LettersAndDigits(input: string): string {
         return input.split('').filter(c => (/[a-zA-Z0-9]/).test(c)).join('')
     }
